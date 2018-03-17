ee0b88a82c69d28ada4b285bd019d1a081b9b1f9
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-17 21:57:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 function theBeatlesPlay(musicians, instruments){
-  var facts = [];
-  for (let i=0; i < musicians.length; i++){
-    facts.push([musicians].i + ' plays ' + [instruments].i);
-    console.log(facts.i);
+  console.log(musicians);
   }
-}
